c138b78f166091599090b876a67952cd190dd6af
Antony Giomar <antonygiomarx@gmail.com>
2023-01-18 23:03:07 -0600

    added handle exceptions service :sparkles:

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,11 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { HandleExceptionsService } from '../shared/handle-exceptions/handle-exceptions.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, HandleExceptionsService],
   imports: [TypeOrmModule.forFeature([Product])],
 })
 export class ProductsModule {}
