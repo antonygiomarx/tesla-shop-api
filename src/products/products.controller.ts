0bd31ab67dc65a2c22f1f9eed951489de0f47842
Antony Giomar <antonygiomarx@gmail.com>
2023-01-20 00:42:56 -0600

    chore: change findOne :art:

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -30,7 +30,7 @@ export class ProductsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productsService.findOne(id);
+    return this.productsService.findOnePlain(id);
   }
 
   @Patch(':id')
