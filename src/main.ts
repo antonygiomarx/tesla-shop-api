caabd9d7dd4a19d30eedb4b5332a5c3d5f246718
Antony Giomar <antonygiomarx@gmail.com>
2023-01-21 13:45:25 -0600

    chore: change main logic to class :art:

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const logger = new Logger('NestApplication');
 
-  app.setGlobalPrefix('api');
+export class NestApplication {
+  public static port = process.env.PORT || 3000;
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  static async start() {
+    const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+    app.useGlobalPipes(new ValidationPipe());
+
+    app.enableCors();
+
+    app.setGlobalPrefix('api');
+
+    await app.listen(this.port);
+  }
 }
 
-bootstrap();
+NestApplication.start()
+  .then(() => {
+    logger.log(`Application listening on port ${NestApplication.port}`);
+  })
+  .catch((error) => {
+    logger.error(error);
+  });
