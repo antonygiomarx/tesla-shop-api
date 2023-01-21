3a94f0feec58aa474d3102a69bcfc7471f9ad39b
Antony Giomar <antonygiomarx@gmail.com>
2023-01-21 14:10:03 -0600

    chore: move static seed data to specific dir :art:

---
+++ src/seed/interfaces/seed.ts
@@ -0,0 +1,19 @@
+export interface SeedProduct {
+  description: string;
+  images: string[];
+  stock: number;
+  price: number;
+  sizes: ValidSizes[];
+  slug: string;
+  tags: string[];
+  name: string;
+  type: ValidTypes;
+  gender: 'men' | 'women' | 'kid' | 'unisex';
+}
+
+export type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
+export type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
+
+export interface SeedData {
+  products: SeedProduct[];
+}
