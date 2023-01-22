e8448e2afdca0e5062bf8b51e8424a2dcf298898
Antony Giomar <antonygiomarx@gmail.com>
2023-01-22 14:35:10 -0600

    chore: added default port :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { UsersModule } from './modules/users/users.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
+      port: +(process.env.DB_PORT || 5432),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
